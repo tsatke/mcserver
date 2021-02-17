e4752e256d80e1496e25425119af6fa6faca09ad
tsatke <tim.satke@gmail.com>
2021-02-17 09:45:04 +0100

    MSER-71 MSER-72 add fuzz function and fuzzbuzz config

--- network/packet/decoder_fuzz.go
+++ network/packet/decoder_fuzz.go
@@ -4,22 +4,27 @@ package packet
 
 import "bytes"
 
-func FuzzDecodeHandshake(data []byte) {
-	fuzzDecode(data, PhaseHandshaking)
-	fuzzDecode(data, PhaseStatus)
-	fuzzDecode(data, PhaseLogin)
-	fuzzDecode(data, PhasePlay)
+func Fuzz(data []byte) int {
+	res := 0
+	res += fuzzDecode(data, PhaseHandshaking)
+	res += fuzzDecode(data, PhaseStatus)
+	res += fuzzDecode(data, PhaseLogin)
+	res += fuzzDecode(data, PhasePlay)
+	if res > 0 {
+		return 1
+	}
+	return 0
 }
 
-func fuzzDecode(data []byte, phase Phase) {
+func fuzzDecode(data []byte, phase Phase) int {
 	p, err := Decode(bytes.NewReader(data), phase)
 	if err != nil {
-		return
+		return 0
 	}
 	if v, ok := p.(Validator); ok {
 		if err := v.Validate(); err != nil {
-			return
+			return 0
 		}
 	}
-	return nil
+	return 1
 }
